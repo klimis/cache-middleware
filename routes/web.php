f9116c80d0ed21e586c9c7fecbbda43ca76e8a83
Klimis <klimiss@gmail.com>
2026-06-02 09:45:23 +0300

    api route

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,8 @@ use Klimis\CacheMiddleware\Http\Controllers\CacherController;
 Route::get('cacher/clear/all', [CacherController::class, 'clearAll']);
 Route::get('cacher/clear/key', [CacherController::class, 'deleteKey']);
 Route::get('cacher/get/all', [CacherController::class, 'index']);
+
+
+Route::get('api/cacher/clear/all', [CacherController::class, 'clearAll']);
+Route::get('api/cacher/clear/key', [CacherController::class, 'deleteKey']);
+Route::get('api/cacher/get/all', [CacherController::class, 'index']);
